7abf8edba056ada6c9f54a27b2da516e15bbadf0
Eric Porter <eporter@scentsy.com>
2016-06-28 11:27:13 -0600

    First commit

--- app.js
+++ app.js
@@ -1,145 +1,44 @@
-    /*var crime = [];
-
-    getCrimeData(crime, function(crimeData) {
-        
-        document.getElementById("output").innerHTML = crimeData;
-        console.log(crimeData);
-    });
-
-  function getCrimeData(crimeData, callback) {
-    var urlBase = 'https://jgentes-Crime-Data-v1.p.mashape.com/crime';
-  var url = urlBase + '?enddate=9%2F25%2F2015&lat=37.757815&long=-122.5076392&startdate=9%2F19%2F2015';
-
-
-    $.get(url, function(data, status) {
-
-				callback({
-					description: description,
-					location: location
-                    
-				});
-               
-			});
-    }*/
-
-    
-
 //Creates and sets default view of map
 var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(39.828151, -98.579512),
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-      });
-    latLng = map.center;
-    //Shows coordinates of center in HTML
-document.getElementById("output").innerHTML = latLng;
-    } 
+var coord;
 
-(function() {
-    $('#zipBtn').click(function() {
-    
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(33.912149, -98.493440),
+        zoom:4,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    latLng = map.center;
+}
+
+//Resets view of map from ZIP code entered
+$('#zipBtn').click(function() {
+
     map.setZoom(12);
 
     var zip = $('#zipcode').val();
 
     getCoord(zip, function(coordData) {
-        map.setCenter(coordData); 
-        document.getElementById("output").innerHTML = map.center;
-        TestMarker();
-      // Function for adding a marker to the page.
-    function addMarker(location) {
-        marker = new google.maps.Marker({
-            position: location,
-            map: map
+        map.setCenter(coordData);
+        document.getElementById("output").innerHTML = coordData.city + ',' + ' ' + coordData.state;
+    });
+
+    //Gets coordinates of zip code entered by user
+    function getCoord(zip, callback) {
+        var urlBase = 'https://api.zippopotam.us/us/';
+        var url = urlBase + zip;
+
+        $.get(url, function(data, status) {
+
+            var place = data.places[0];
+
+            callback({
+                lat: Number(place['latitude']),
+                lng: Number(place['longitude']),
+                city: place['place name'],
+                state: place['state']
+            });
         });
     }
+});
 
-    // Testing the addMarker function
-    function TestMarker() {
-           center = map.center;
-           addMarker(center);
-    }        
-		});
-
-        getCityData(zip, function(cityData) {
-       
-        document.getElementById("city-data").innerHTML = zips.city;
-        TestMarker();       
-		});
-    
-    //Gets coordinates of zip code entered by user
-function getCoord(zip, callback) {
-    var urlBase = 'https://api.zippopotam.us/us/';
-    var url = urlBase + zip;
-
-    $.get(url, function(data, status) {
-
-				var place = data.places[0];
-
-				callback({
-					lat: Number(place['latitude']),
-					lng: Number(place['longitude'])
-                    
-				});
-			});
-    }
-
-    function getCityData(zip, callback) {
-    var urlBase = "https://zipfeeder.us/zip?key=thXpN5j-&zips=";
-    var url = urlBase + zip;
-
-    $.get(url, function(data, status) {
-
-				var place = data.places[0];
-
-				callback({
-					cit: (place['city']),
-                    met: cbsa_name
-                    
-				});
-                console.log(getCityData);
-			});
-            
-    }
-
-    });
-   /* var app = angular.module('ngCrime', []);
-
-    app.controller('crimeController', ['guidService', function(locationService, guidService) {
-
-        var crime = this;
-
-        crime.location = [];
-        crime.newEntry = {
-            description: '',
-            datetime: '',
-            location: ''
-    }
-
-        crime.addStory = function() {
-            guidService.getGuid().then(
-                function (guid) {
-                var newEntry = angular.copy(crime.newEntry);
-
-                
-                crime.zipCode.description = '';
-                crime.zipCode.datetime = '';
-                crime.zipCode.location = '';
-                }
-            );    
-        };
-
-
-    }]);
-*/
-})();
-
-
-
-
-
-
-
-    
---
+++ marker.js
@@ -0,0 +1,86 @@
+var offset = [];
+//offset 982 includes Idaho schools
+var getOffset = (function() {
+    for (s = 0; s <= 7769; s++) {
+        offset.push(s);
+    }
+})();
+
+var urlBase = 'https://inventory.data.gov/api/action/datastore_search?offset='
+var resourceID = '&resource_id=38625c3d-5388-4c16-a30f-d105432553a4';
+
+var urlArray = [];
+for (u = 0; u <= 7769; u++) {
+    urlS = urlBase + offset[u] + resourceID;
+    urlArray.push(urlS);
+}
+
+var url;
+for (r = 0; r <= 7769; r+=100) {
+    url = urlArray[r];
+    
+
+    var getSchools = $.getJSON(url, function(data) {
+
+        /* loop through array */
+        getCoord = $.each(data, function(index, d) {
+
+            if (!d.records) return;
+
+            for (i = 0; i < d.records.length; i++) {
+
+                Lat = Number(d.records[i].LATITUDE);
+                Lng = Number(d.records[i].LONGITUD);
+                name = d.records[i].INSTNM;
+                address = d.records[i].ADDR;
+                city = d.records[i].CITY;
+                state = d.records[i].STABBR;
+                zip = d.records[i].ZIP;
+                website = d.records[i].WEBADDR;
+
+                //Excludes smaller institutions
+                size = Number(d.records[i].INSTSIZE);
+                if (size <= 2) {
+                    continue
+                };
+
+                console.log(name);
+
+                ColMarker();
+            }
+        });
+    });
+}
+
+var infowindow;
+
+function addMarker(LatLng) {
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(Lat, Lng),
+       icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 4,
+            strokeWeight: 1,
+            fillOpacity:1,
+            fillColor: 'red'
+          },
+        map: map
+    });
+    var contentString = '<h2>' + name + '</h2>' +
+        '<p>' + address + '<br>' + city + ',' + ' ' + state + ' ' + zip + '</p>' +
+        '<p><a href="http://' + website + '" target="_blank">' + website + '</a></p>';
+
+
+    marker.addListener('click', function() {
+        if (infowindow) infowindow.close();
+        infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        infowindow.open(map, marker);
+        infowindow.setPosition(this.position);
+    });
+}
+// Testing the addMarker function
+function ColMarker() {
+    addMarker(map);
+}
