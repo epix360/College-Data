663e704bc035faa11534d52901238178b8c57999
Eric Porter <eporter@scentsy.com>
2016-06-28 11:38:40 -0600

    Security tweak

--- marker.js
+++ marker.js
@@ -6,7 +6,7 @@ var getOffset = (function() {
     }
 })();
 
-var urlBase = 'http://cors.io/?u=https://inventory.data.gov/api/action/datastore_search?offset='
+var urlBase = 'https://cors.io/?u=https://inventory.data.gov/api/action/datastore_search?offset='
 var resourceID = '&resource_id=38625c3d-5388-4c16-a30f-d105432553a4';
 
 var urlArray = [];
